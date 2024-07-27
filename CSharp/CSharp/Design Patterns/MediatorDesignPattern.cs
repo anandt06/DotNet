bfe905df0a9cf59923c72999929db775562db59f
Anand T <anand.t@siemens.com>
2024-07-27 19:13:09 +0530

    mediator design pattern modified

--- CSharp/CSharp/Design Patterns/MediatorDesignPattern.cs
+++ CSharp/CSharp/Design Patterns/MediatorDesignPattern.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            IMediator mediator = new ConcreteMediator();
-            IParticipant participant1 = new ConcreteParticipant1(mediator);
-            IParticipant participant2 = new ConcreteParticipant2(mediator);
+            IChatMediator mediator = new ChatMediator();
+            IParticipant participant1 = new Participant1(mediator, "Participant 1");
+            IParticipant participant2 = new Participant2(mediator, "Participant 2");
             mediator.AddParticipant(participant1);
             mediator.AddParticipant(participant2);
             participant1.SendMessage("This is the first participant");
@@ -14,14 +14,14 @@
             Console.ReadLine();
         }
     }
-    public interface IMediator
+    public interface IChatMediator
     {
         void AddParticipant(IParticipant participant);
 
         void BroadcastMessage(string message, IParticipant sender);
     }
 
-    public class ConcreteMediator : IMediator
+    public class ChatMediator : IChatMediator
     {
         List<IParticipant> participants = new List<IParticipant>();
         public void AddParticipant(IParticipant participant)
@@ -30,33 +30,59 @@
         }
         public void BroadcastMessage(string message, IParticipant sender)
         {
-            // Write code here to broadcast the message to the participants
+            foreach (var participant in participants)
+            {
+                if (participant != sender)
+                {
+                    participant.ReceiveMessage(message);
+                }
+            }
         }
     }
 
     public interface IParticipant
     {
         void SendMessage(string message);
+
+        void ReceiveMessage(string message);
     }
-    public class ConcreteParticipant1 : IParticipant
+    public class Participant1 : IParticipant
     {
-        protected IMediator _mediator;
-        public ConcreteParticipant1(IMediator mediator)
+        protected IChatMediator _mediator;
+
+        private string _name;
+        public Participant1(IChatMediator mediator, string name)
         {
             _mediator = mediator;
+            _name = name;
         }
+
+        public void ReceiveMessage(string message)
+        {
+            Console.WriteLine($"{_name} receives: {message}");
+        }
+
         public void SendMessage(string message)
         {
             _mediator.BroadcastMessage(message, this);
         }
     }
-    public class ConcreteParticipant2 : IParticipant
+    public class Participant2 : IParticipant
     {
-        protected IMediator _mediator;
-        public ConcreteParticipant2(IMediator mediator)
+        protected IChatMediator _mediator;
+
+        private string _name;
+        public Participant2(IChatMediator mediator, string name)
         {
             _mediator = mediator;
+            _name = name;
         }
+
+        public void ReceiveMessage(string message)
+        {
+            Console.WriteLine($"{_name} receives: {message}");
+        }
+
         public void SendMessage(string message)
         {
             _mediator.BroadcastMessage(message, this);
